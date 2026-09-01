e9c9725f7f048f8769c1b2f8ce5896f18db48a04
Atiqul Islam <liadbd20@gmail.com>
2026-09-01 22:25:28 +0600

    Fix missing import in stockholder_home_screen.dart

--- lib/features/auth/presentation/screens/stockholder_home_screen.dart
+++ lib/features/auth/presentation/screens/stockholder_home_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
+import '../../../../core/config/supabase_config.dart';
 import '../../../../core/services/api_service.dart';
 import '../../../marketplace/presentation/screens/order_chat_screen.dart';
 import 'incoming_order_screen.dart';
